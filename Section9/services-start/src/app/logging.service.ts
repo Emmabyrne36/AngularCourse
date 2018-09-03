88d248fd26f4859ebc575dd36108f854a2791d42
Emmabyrne36 <Emmabyrne36@gmail.com>
2018-09-03 15:28:44 +0100

    Removed deleted folders

--- Section9/services-start/src/app/logging.service.ts
+++
@@ -1,6 +0,0 @@
-// Service is a normal typescript class
-export class LoggingService {
-    logStatusChange(status: string) {
-        console.log('A server status changed, new status: ' + status);
-    }
-}
